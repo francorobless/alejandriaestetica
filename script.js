d3145e76b31bbdb739eeadeccf197eeb8102e202
francoroblesss <francorobles06@gmail.com>
2023-07-05 13:00:02 -0300

    archivos subidos

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-let total = 0;
+//agregar al carrito
+let articulos = [];
+let precio = [];
 
-let agregarAlcarrito = (nombreProducto, precioProducto) => {
- 
-    total += precioProducto;
-
-    console.log(nombreProducto)
-    console.log(total)
+agregarAlcarrito = (nombreProducto, precioProducto) => {
+  articulos.push(nombreProducto);
+  precio.push(precioProducto);
+  console.log(articulos);
+  sumarPrecios();
 }
+
+sumarPrecios = () => {
+    let suma = precio.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    console.log(suma);
+
+    let articulosCarrito = document.getElementById('articulosCarrito');
+    articulosCarrito.textContent = "Artículos en el carrito: " + articulos.join(", ");
+  
+    let totalCarrito = document.getElementById('totalCarrito');
+    totalCarrito.textContent = "Total: " + suma; 
+}
+
+
+//desplegar div
+let botonDesplegar = document.getElementById('botonDesplegar');
+let contenidoDesplegable = document.getElementById('contenidoDesplegable');
+
+botonDesplegar.addEventListener('click', () => {
+  if (contenidoDesplegable.style.display === 'none') {
+    contenidoDesplegable.style.display = 'block';
+  } else {
+    contenidoDesplegable.style.display = 'none';
+  }
+});
